90080a553e2598beada630e46601c52180207929
rathD25 <61065540+rathD25@users.noreply.github.com>
2022-01-17 13:17:49 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ read username
 echo "Adding user $username"
 adduser $username
 usermod -aG sudo $username
-su $username
-sudo apt update && sudo apt upgrade
-sudo dpkg-reconfigure locales
-sudo apt install zsh
+dpkg-reconfigure locales
+apt update && sudo apt upgrade
+apt install zsh
+su -m $username
+echo "You should be within the $username shell"
